0ef76aa73a17a6175a762d7209c26457e4d8a8d7
eisti <eisti@julien-bc>
2020-04-04 15:30:46 +0200

    Me voila arrive

--- src/control/main/Main.java
+++ src/control/main/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Ceci est un premier commit pour faire des tests...");
 		System.out.println("Coucou");
+		System.out.println("Me voila");
 	}
 
 }
