8bc27fb670be2f727cefb3f8ed1076d3686350ef
sonicpierre <pvirgaux@gmail.com>
2020-04-10 00:36:14 +0200

    Securité...

--- src/control/main/Main.java
+++ src/control/main/Main.java
@@ -5,7 +5,7 @@ import graphic.fenetre.FenetreLogin;
 public class Main {
 
 	public static void main(String[] args) {
-		//Voilà la fenètre graphique sans rien dedans.
+		//Voilà la fenètre graphique sans rien dedans. Changements...
 		FenetreLogin.getInstance();
 		MoteurGraphique.getInstance();
 	}
