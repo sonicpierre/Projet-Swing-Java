d50ed35219ad979c6df2f37307f3f5d3831e548a
sonicpierre <pvirgaux@gmail.com>
2020-04-12 23:38:40 +0200

    Debut autre menu

---
+++ src/graphic/menusParametreFenetre/MenuReparametrageDuCompte.java
@@ -0,0 +1,15 @@
+package graphic.menusParametreFenetre;
+
+import java.awt.GridLayout;
+
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class MenuReparametrageDuCompte extends JPanel{
+
+	
+	public MenuReparametrageDuCompte() {
+		this.setLayout(new GridLayout(3, 3, 20, 0));
+	}
+	
+}
