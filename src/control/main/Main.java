1bb7972ca6e7e745fd3970d74470def657c25084
eisti <eisti@student-laptop>
2020-04-20 10:31:19 +0200

    J'ai rajouter un commentaire

--- src/control/main/Main.java
+++ src/control/main/Main.java
@@ -7,6 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		//Voilà la fenètre graphique sans rien dedans. Voilà voilà voilà
 		FenetreLogin.getInstance();
+		//Ceci est un test
 	}
 
 }
